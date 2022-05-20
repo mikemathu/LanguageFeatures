2e9cd7df95f89650009c00d4891fdfc431977299
Michael Mathu <mathumichael17@gmail.com>
2022-05-20 19:23:09 +0300

    created controller and view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,11 @@
-﻿using LanguageFeatures.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-
+﻿using Microsoft.AspNetCore.Mvc;
 namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public ViewResult Index()
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new string[] { "C#", "Language", "Features" });
         }
     }
 }
